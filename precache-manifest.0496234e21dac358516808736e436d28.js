3e6330023c097c617477248a28878873150497f3
AndreyDmitriew <46495940+AndreyDmitriew@users.noreply.github.com>
2020-01-04 02:25:58 +0300

    Add files via upload

---
+++ precache-manifest.0496234e21dac358516808736e436d28.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11e4652c59df79eff00f4efcc250148c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "be6a629fa0089422f929",
+    "url": "/static/css/main.c18df38f.chunk.css"
+  },
+  {
+    "revision": "88ef087707047244ac20",
+    "url": "/static/js/2.65dd199b.chunk.js"
+  },
+  {
+    "revision": "be6a629fa0089422f929",
+    "url": "/static/js/main.72c781f0.chunk.js"
+  },
+  {
+    "revision": "aedf08bfd231bc1f44d7",
+    "url": "/static/js/runtime~main.7677ad42.js"
+  }
+]);
